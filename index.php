c85ddc0ca6988fbfe5b3eebf61bd1774314fa28b
ceb5002 <ceb5002@rit.edu>
2018-02-02 23:37:54 -0500

    php test 2

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php include('head.php'); ?>
 <?php include('navbar.php'); ?>
-<body></body>
+<body>
+	<h1>testing 123</h1>
 <div class="main">
 	<h1>Heading test</h1>
 	<p>paragraph test paragraph test paragraph test paragraph test paragraph testparagraph testparagraphparagraph testparagraph testparagraph testparagraph testparagraph testparagraph testparagraph testparagraph testparagraph testparagraph paragraph test paragraph test paragraph test paragraph test paragraph test paragraph test paragraph testparagraph testparagraph testparagraph testparagraph testparagraphparagraph paragraph testparagraph testparagraph testparagraph testparagraph testparagraph testparagraph testparagraph testvparagraph testparagraph testparagraph testparagraph testvparagraph testvparagraph testparagraph testparagraph v v paragraph testvvparagraph testparagraph testvparagraph vparagraph testvvparagraph testvparagraph vvvvparagraph testparagraph testparagraph testparagraph testparagraph testvvvparagraph testvvvv v paragraph testparagraph testparagraph test</p>
