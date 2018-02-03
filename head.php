f2e1d196dc611eac0b1866ee448c00116e7f5b95
ceb5002 <ceb5002@rit.edu>
2018-02-02 23:32:47 -0500

    php test?

---
+++ head.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<head>
+	<link rel="stylesheet" href="index.css">
+	<title>It's all about Caitlin</title>
+</head>
---
+++ navbar.php
@@ -0,0 +1,14 @@
+<div class="topnav" id="navbar">
+	<a class="active" href="index.html">Home</a>
+	<a class="navbtn" href="contact.html">Contact</a>
+	<a class="navbtn" href="#about">About</a>
+
+	<div class="dropdown">
+		<a class="navbtn" href="#projects">Projects</a>
+		<div class="dropdown-content">
+			<a href="#1">1</a>
+			<a href="#2">2</a>
+			<a href="#3">3</a>
+		</div>
+	</div>
+</div>
