fd171134ec9576877a8eb87524ec4fbd4c4c2d98
ceb5002 <ceb5002@rit.edu>
2018-02-02 23:59:41 -0500

    php test didn't work

--- head.php
+++
@@ -1,8 +0,0 @@
-<!DOCTYPE html>
-<html>
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<head>
-	<link rel="stylesheet" href="index.css">
-	<title>It's all about Caitlin</title>
-</head>
